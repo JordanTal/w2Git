0c66779213ce3ffc7e80646fa35f84d477412514
yarden tal <jordan23.tal@gmail.com>
2021-09-12 01:20:49 +0300

    removed debug print , improved readability , and comments were added as well

--- password-validator.sh
+++ password-validator.sh
@@ -1,51 +1,50 @@
 #!/bin/bash
 
+# Read passed password (as Default when no flag being used)
 pass=$1
+echo 
+echo "- checking that your password is valid , see results below - "
+echo 
+# Check for File flag(s) section : 
 while getopts f: option
-do
-case "${option}"
-in
-f )
- PASS_FILE=${OPTARG}
- pass=$(head -n 1 $PASS_FILE)
- ;;
+	do
+	case "${option}"
+		in
+		f )	# Password file to read from
+			PASS_FILE=${OPTARG}
+			pass=$(head -n 1 $PASS_FILE)
+			;;
+		\?) # Invalid option
+			echo "Error: Invalid option"
+			exit 1;;
 esac
 done
-
-
-#if [[ $1 -eq "-f" ]]
-#then	
-#	pass=$(head -n 1 $2)
-#	echo "$pass"	
-#else
-#	pass=$1
-#fi
+# Set default password validity status to true
 validity=true
-echo password entered is $pass
+# password length check
 if [[ !( "${#pass}" -ge 10 ) ]]
-then
-    echo password too short , password needs must contain at least 10 characters
-	#exit 1
+  then
+    echo .. -_- Password is too short , password must contain at least 10 characters .. -_-
+	# Set validity status to false
 	validity=false
-	echo calidy status is $validity
 fi
+# Digit existance check
 if [[ !("$pass" == *[0-9]* )]]
-then
-	echo invalid password, password should contain at least 1 digit
-	#exit 1 	
+  then
+	echo ... -_- Password MUST contain at least 1 digit... -_- 
 	validity=false
-	echo calidy status is $validity
 fi
+# Characters existance (Upper & Lower) check
 if [[ !(("$pass" == *[A-Z]*) && ("$pass" == *[a-z]*))]]
-then
-	echo Invalid password ,password should include both the small and capital case letters.
-	#exit 1
+  then
+	echo .... -_- Password MUST include both the small and capital case letters .... -_- 
 	validity=false
-	echo calidy status is $validity
 fi
+# Exit when password found invalid due to at least one of the conditions
 if [ $validity = false ]
-then
+  then
 	exit 1
+# Feedback for valid password which meets the reuirements
 else
 	echo "password is valid (-: thanks"
 fi
