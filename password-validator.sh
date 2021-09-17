9b138a9649304c4ecb158c36a176768e059b90fd
yarden tal <jordan23.tal@gmail.com>
2021-09-17 16:57:49 +0300

    UX imrovements (output colors included)

--- password-validator.sh
+++ password-validator.sh
@@ -24,20 +24,20 @@ validity=true
 # password length check
 if [[ !( "${#pass}" -ge 10 ) ]]
   then
-    echo .. -_- Password is too short , password must contain at least 10 characters .. -_-
+    echo "$(tput setaf 1).. -_- Password is too short , password must contain at least 10 characters .. -_-"
 	# Set validity status to false
 	validity=false
 fi
 # Digit existance check
 if [[ !("$pass" == *[0-9]* )]]
   then
-	echo ... -_- Password MUST contain at least 1 digit... -_- 
+	echo "$(tput setaf 1)... -_- Password MUST contain at least 1 digit... -_- "
 	validity=false
 fi
 # Characters existance (Upper & Lower) check
 if [[ !(("$pass" == *[A-Z]*) && ("$pass" == *[a-z]*))]]
   then
-	echo .... -_- Password MUST include both the small and capital case letters .... -_- 
+	echo "$(tput setaf 1).... -_- Password MUST include both the small and capital case letters .... -_- "
 	validity=false
 fi
 # Exit when password found invalid due to at least one of the conditions
@@ -46,6 +46,6 @@ if [ $validity = false ]
 	exit 1
 # Feedback for valid password which meets the reuirements
 else
-	echo "password is valid (-: thanks"
+	echo "$(tput setaf 2)Password is valid (-: thanks"
 fi
 
