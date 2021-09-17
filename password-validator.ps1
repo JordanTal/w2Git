5c124ebe9ef73397a085a05915dea4852afec451
yarden tal <jordan23.tal@gmail.com>
2021-09-17 17:59:27 +0300

    removed wrong powershell script , and renamed the correct one ('untitled2.ps1' - to the rigth name 'password-validator.ps1' per the instructtions.

--- password-validator.ps1
+++ password-validator.ps1
@@ -1,11 +1,10 @@
 ﻿clear-host
- 
+
+# Put the first argument into the password variable 
 $pass = $Args[0]
 
 # Set default validity to True , if any condition does not meet - will turn to false.
 $pass_validity = $true
-
-"First argument is " +$pass
 
 write-host
 
@@ -13,42 +12,45 @@ write-host
 
 write-host 
 
+# If flag -f was truned on 
 if ($pass -eq '-f')
 {
     write-host "flag -f tuned on"
     $filePath = $Args[1]
     "reading password from file" + $filePath
     write-host
+    # Place the passwor from file into the password variable
     $pass =  get-content "$filePath" | select -first 1
-      
-      write-host "pass is: $pass"
 }
 
-write-host
-write-host $pass
-
-
+# Check password's length
 if (!($pass.length -ge 10))
 {
-     Write-Host "`n pass MUST have at least 10 charachters )-: `n "
+     Write-Host "`n ! password MUST have at least 10 charachters `n "
      $pass_validity = $false
 }
+# Check that the password uses both lower and upper case charachters
 if(!(($pass -cmatch '[a-z]') -and ($pass -cmatch '[A-Z]')))
 { 
-    Write-Host "`n pass must have at least ONE Lower case and ONE Upper case characters in it ! )-: `n "
+    Write-Host "`n !! password must have at least ONE Lower case and ONE Upper case characters in it !  `n "
          $pass_validity = $false
-} 
+}
+# Check that password includes at least one digit 
 if(!($pass -match '\d'))
 {
-     Write-Host "`n  pass must contain At least one digit! )-: `n "
+     Write-Host "`n !!! password must contain At least one digit! `n "
           $pass_validity = $false
 }
 
+# If at least one of the reuirements does not meet 
 if ($pass_validity -eq $false)
 {
+    Write-Host "`n`n`n)-: As seen above - password does not meet at least one of the requirements... `n"
     exit
 }
-else
+# If password passed the test ( does meet all the reuirements)
+else 
 { 
      Write-Host "`nCongrats! - pass does meet the requirements!`n"
+     exit
 }
